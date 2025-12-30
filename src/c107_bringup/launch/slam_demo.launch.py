acad6c0c5dbbee1dc626c0d5981dc6b5d0387ca7
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2025-12-30 23:04:05 +0900

    [modify] launch

--- src/c107_bringup/launch/slam_demo.launch.py
+++ src/c107_bringup/launch/slam_demo.launch.py
@@ -7,8 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 import os
 
-pkg_dir = get_package_share_directory("bringup")
-glim_dir = get_package_share_directory("glim")
+pkg_dir = get_package_share_directory("c107_bringup")
 
 def generate_launch_description():
     list = [
@@ -22,35 +21,19 @@ def generate_launch_description():
             ],
         ),
         Node(
-            package="joy",
-            executable="joy_node",
-            namespace="",
+            package="picomni_rover_ros", 
+            executable="picomni_rover_ros", 
+            namespace=""
         ),
-        Node(
-            package="omni_teleop",
-            executable="omni_teleop_node",
-            namespace="",
-        ),
-        # Node(
-        #     package="rviz2",
-        #     executable="rviz2",
-        #     namespace="",
-        #     arguments=["-d" + os.path.join(pkg_dir, "rviz", "whill_e2e_nav.rviz")],
-        # ),
-        Node(package="picomni_rover_ros", executable="picomni_rover_ros", namespace=""),
-        # Node(
-        #     package="glim_ros",
-        #     executable="glim_rosnode",
-        #     namespace="",
-        #     parameters=[
-        #         {
-        #             "config_path" : os.path.join(glim_dir, "config")
-        #         }
-        #     ]
-        # )
         Node(
             package="lidar_wheel_slam_demo",
             executable="lidar_wheel_slam_demo",
+            namespace="",
+            output="screen",
+        ),
+        Node(
+            package="teleop_twist_keyboard",
+            executable="teleop_twist_keyboard",
             namespace="",
             output="screen",
         )
